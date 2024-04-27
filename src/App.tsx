5be805ab19631a118af52e2c147d1a6f3b457e1a
Junmar <junmarr30@gmail.com>
2024-04-27 23:02:32 +0800

    updatingg object and array

--- src/App.tsx
+++ src/App.tsx
@@ -57,16 +57,84 @@
 
 // export default App;
 
-import Like from './components/like';
+//LIKE BUTTON
+// import Like from './components/like';
+
+// function App() {
+//   return (
+//     <div>
+//       <Like
+//         onLike={() => {
+//           console.log('click');
+//         }}
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//State PERSON
+// import { useState } from 'react';
+
+// function App() {
+//   const [person, setPerson] = useState({
+//     firstName: 'Junmar',
+//     lastName: 'Rosario',
+//   });
+
+//   const fullName = `${person.firstName} ${person.lastName}`;
+
+//   return <div>{fullName}</div>;
+// }
+
+// export default App;
+
+//updating object
+// import { useState } from 'react';
+
+// function App() {
+//   const [drink, setDrink] = useState({
+//     drink: 'Red Horse',
+//     price: 55,
+//   });
+
+//   const addPrice = () => {
+//     setDrink({ ...drink, price: 100 });
+//   };
+
+//   return (
+//     <div>
+//       <div>price: {drink.price}</div>
+//       <button onClick={addPrice}>Click to add price</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import { useState } from 'react';
 
 function App() {
+  const [love, setLove] = useState(['Junmar', 'Carmina']);
+
+  const handleClick = () => {
+    //Add new array item
+    setLove([...love, 'love']);
+    console.log(love);
+
+    //remove array item
+    setLove(love.filter((tag) => tag !== 'Junmar'));
+    console.log(love);
+
+    //update array item
+    setLove(love.map((tag) => (tag === 'Carmina' ? 'Carmina Rosario' : tag)));
+    console.log(love);
+  };
+
   return (
     <div>
-      <Like
-        onLike={() => {
-          console.log('click');
-        }}
-      />
+      <button onClick={handleClick}>click</button>
     </div>
   );
 }
